c5962bbde71c885d43f954020fd372de2e4186d0
liu2015 <176784830@qq.com>
2021-08-03 13:33:45 +0800

    上传一个数据库文件

--- data/MySQL____localhost-2021_08_03_13_19_21-dump.sql
+++ data/MySQL____localhost-2021_08_03_13_19_21-dump.sql
@@ -19,6 +19,8 @@
 -- Table structure for table `auth_rule`
 --
 
+
+
 DROP TABLE IF EXISTS `auth_rule`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
